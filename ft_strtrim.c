180eb7e923a1dbc4af24a09c35bb39be465904c7
root <root@LAPTOP-4IHF73HC.localdomain>
2022-04-22 15:06:30 +0800

    strtim test

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -17,26 +17,20 @@ char	*ft_strtrim(char const *s1, char const *set)
 	size_t beg;
 	size_t end;
 	size_t ctr;
-	char *str;
 
 	end = ft_strlen(s1);
 	beg = 0;
 	ctr = 0;
 	if (set != 0 && s1 != 0)
 	{
-		while (s1[beg] == ' ' || ft_strchr(s1, (int)set))
+		while (s1[beg] == ' ' || ft_strchr(s1, (unsigned int)set))
 			beg++;
-		while (s1[end] == ' ' || ft_strchr(s1, (int)set))
+		while (s1[end] == ' ' || ft_strchr(s1, (unsigned int)set))
 			end--;
-		str = (char *)malloc (beg);
 		while (end > beg && s1[beg] != '\0')
 		{
-			str[ctr] = s1[beg];
-			ctr++;
-			beg++;
-		} 
-		str[ctr] = '\0';
-		return (str);
+			return (ft_substr(s1, 0, ft_strlen(s1) + 1));
+		}
 	}
 	return (0);
 }
