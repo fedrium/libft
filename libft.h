317a35630c6f4a8770c8ccca74544ab628cbef65
Yu Xiang <99579760+fedrium@users.noreply.github.com>
2022-05-09 16:42:13 +0800

    Add files via upload

--- libft.h
+++ libft.h
@@ -1,9 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cyu-xian <cyu-xian@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/09 12:11:01 by cyu-xian          #+#    #+#             */
+/*   Updated: 2022/05/09 16:36:44 by cyu-xian         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef LIBFT_H
 #	define LIBFT_H
 
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+
+typedef	struct		s_list
+{
+	void 			*content;
+	struct	s_list 	*next;
+} 					t_list;
 
 int	ft_atoi(const char *str);
 void	ft_bzero(void *s, size_t n);
@@ -15,7 +33,7 @@ int	ft_isprint(int c);
 void	*ft_memchr(const void *s, int c, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t n);
 int	ft_memcmp(const void *s1, const void *s2, size_t n);
-void	ft_memcpy(void *dst, const void *src, size_t n);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
 char 	*ft_memset(void *b, int c, size_t n);
 char	*ft_strchr(const char *s, int c);
 int	ft_strncmp(const char *s1, const char *s2, size_t n);
@@ -35,5 +53,13 @@ char	*ft_itoa(int n);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
+char	*ft_strmapi(char const *s, char (*f)(unsigned
+int, char));
+void	ft_striteri(char *s, void (*f)(unsigned int,
+char*));
+char	**ft_split(char const *s, char c);
+t_list *ft_lstnew(void *content);
+void	ft_lstadd_front(t_list **lst, t_list *new);
 
 #endif
