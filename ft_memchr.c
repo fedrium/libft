8c6b921b2dfee13a7c2cfc59fb6490960800fefc
Yu Xiang <99579760+fedrium@users.noreply.github.com>
2022-05-19 14:54:38 +0800

    Add files via upload

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: cyu-xian <cyu-xian@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 15:59:52 by cyu-xian          #+#    #+#             */
-/*   Updated: 2022/05/09 11:38:09 by cyu-xian         ###   ########.fr       */
+/*   Updated: 2022/05/17 11:12:23 by cyu-xian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,14 @@
 void	*ft_memchr(const void *s, int c, size_t n)
 {
 	size_t	i;
-	char	*k;
 
 	i = 0;
-	k = (char *)s;
-	if (n == 0)
-		return (0);
-	if (c > 256)
-		c = c - 256;
-	while (n > i)
+	while (i < n)
 	{
-		if ((int)k[i] == c)
-			return (&k[i]);
-		else
-			i++;
+		if (*(unsigned char *)s == (unsigned char)c)
+			return ((void *)s);
+		i++;
+		s++;
 	}
 	return (0);
 }
